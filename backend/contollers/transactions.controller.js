2067bc047a749979b5ae68863a450f529af4add6
Jesus <r.jesus9107@gmail.com>
2022-02-10 23:43:13 -0800

    transactions - asyncHandler

--- backend/contollers/transactions.controller.js
+++ backend/contollers/transactions.controller.js
@@ -1,51 +1,66 @@
+import asyncHandler from 'express-async-handler';
 import Transaction from "../models/transaction.model.js";
 
 // @desc    Get transactions
 // @route   GET /api/v1/transactions
 // @access  Private
-export const getTransactions = (req, res) => {
-    Transaction.find()
-        .then(transactions => res.json(transactions))
-        .catch(err => res.status(400).json("Error: " + err));
-}
+export const getTransactions = asyncHandler(async (req, res) => {
+  const transactions = await Transaction.find();
+
+  res.status(200).json(transactions);
+})
 
 // @desc    Set transactions
 // @route   POST /api/v1/transactions
 // @access  Private
-export const setTransactions = async (req, res) => {
-    const transaction = new Transaction({ 
-        description: req.body.description, 
-        date: Date.parse(req.body.date), 
-        amount: req.body.amount, 
-        type: req.body.type, 
-        category: req.body.category, 
-        account: req.body.account
-    });
-    
-    transaction.save()
-        .then(transaction => res.json(transaction))
-        .catch(err => res.status(400).json("Error: " + err));
-}
+export const setTransactions = asyncHandler(async (req, res) => {
+  if (!req.body.description && !req.body.date && !req.body.amount && !req.body.type && !req.body.category && !req.body.account) {
+    res.status(400);
+    throw new Error("Please add all required fields");
+  }
+
+  const transaction = await Transaction.create({
+    description: req.body.description,
+    date: Date.parse(req.body.date),
+    amount: req.body.amount,
+    type: req.body.type,
+    category: req.body.category,
+    account: req.body.account
+  });
+
+  res.status(200).json(transaction);
+})
 
 // @desc    Update transactions
 // @route   PUT /api/v1/transactions/:id
 // @access  Private
-export const updateTransaction = async (req, res) => {
-    const transaction = await Transaction.findOne({ _id: req.params.id });
-    for (let [key, val] of Object.entries(req.body)) {
-        transaction[key] = val;
-    };
+export const updateTransaction = asyncHandler(async (req, res) => {
+  const transaction = await Transaction.findById(req.params.id);
 
-    transaction.save()
-        .then(() => res.json(transaction))
-        .catch(err => res.status(400).json("Error: " + err));
-}
+  if (!transaction) {
+    res.status(400);
+    throw new Error("Transaction not found");
+  }
+
+  const updatedTransaction = await Transaction.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,    
+  });
+
+  res.status(200).json(updatedTransaction);
+})
 
 // @desc    Delete transactions
 // @route   DELETE /api/v1/transactions/:id
 // @access  Private
-export const deleteTransaction = (req, res) => {
-    Transaction.findByIdAndDelete({ _id: req.params.id })
-        .then(() => res.json("deleted"))
-        .catch(err => res.status(400).json("Error: " + err));
-}
+export const deleteTransaction = asyncHandler(async (req, res) => {
+  const transaction = await Transaction.findById(req.params.id);
+
+  if (!transaction) {
+    res.status(400);
+    throw new Error("Transaction not found");
+  }
+
+  await transaction.remove();
+
+  res.status(200).json({ id: req.params.id });
+})
