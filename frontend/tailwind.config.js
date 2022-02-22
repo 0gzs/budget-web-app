3174db019caa8a34928c0a94b6401ab06fd6cfa5
Jesus <r.jesus9107@gmail.com>
2022-02-21 17:34:44 -0800

    configured tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
-  },
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   variants: {extend: {backgroundColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],textColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],    }  },
   theme: {
     extend: {
@@ -12,18 +10,7 @@ module.exports = {
         carbontext: "#9CA3AF",
         carbonlight: "#323a42",
         carbonblue: "#393f47",
-        moneygreen: "#23d183",
-        moon: "#A8A9AE",
-        redrock: "#BA6655",
-        juniper: "#54876B",
-        darkgreen: "#06D48B",
-        blackberry: "#875470",
-        emeraldgreen: "#50C878",
-        apricot: "#DE6353",
-        yellow: "#ffcb2b",
-        eucalyptus: "#b985f9",
-        glossy: "#4ACEC1",
-        firm: "#C88FB4"
+        moneygreen: "#23d183"
       },
       fontFamily: {
         source: ["'Source Sans Pro'", 'sans-serif']
